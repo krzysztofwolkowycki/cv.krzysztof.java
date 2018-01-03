cb02fab6a5e2a2aaae4beceeabc4dfbbcab07219
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2018-01-03 21:05:55 +0100

    Maly początek

--- js/main.js
+++ js/main.js
@@ -7,3 +7,22 @@ function normalImg(x) {
     x.style.height = "150px";
     x.style.width = "150px";
 }
+
+function zaloguj() {
+    var imie = document.getElementById("head");
+    var stanowisko = document.getElementById("head2");
+    imie.innerHTML = "Krzysztof Wolkowycki";
+    stanowisko.innerHTML = "Kierownik dzialu mechaniczno - elektrycznego";
+}
+var fota = document.getElementById("zdjecie");
+fota.onmouseover = podswietl;
+fota.onmouseout = wygas;
+
+function podswietl(ev) {
+    fota.scr = 
+ 
+}
+function wygas(ev) {
+
+    ev.target.style.background = null;
+}
